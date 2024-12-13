1f830fadd772853bea16b29b4a170b2a8b3ae511
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-12-13 20:56:12 +0000

    feat: cleanup on aisle 3 (ingester)

--- apps/ingester/src/config.ts
+++ apps/ingester/src/config.ts
@@ -1,13 +1,6 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  PORT: z.coerce.number().lte(65535).default(8080),
-  HOST: z.string().ip().default('0.0.0.0'),
-
-  PUBLIC_DIR: z.string().default('./public'),
-
-  CORS_ORIGINS: z.array(z.string()).default(['http://localhost:5173', 'https://cookware.dev.hayden.moe']),
-
   TURSO_CONNECTION_URL: z.string().default('https://turso.dev.hayden.moe'),
   TURSO_AUTH_TOKEN: z.string().or(z.undefined()),
 
@@ -16,11 +9,6 @@ const envSchema = z.object({
     .url()
     .default('wss://jetstream1.us-east.bsky.network/subscribe'),
   PLC_DIRECTORY_URL: z.string().url().default('https://plc.directory'),
-
-  JWKS_PRIVATE_KEY: z.string().default('{"kty":"EC","x":"pew2xWIyBQ4XSY4gcCuTJBI-oC5rQqQlcDxIN8nN834","y":"aiJFNEFWyKKWGiFKPRvLAU4wdhsfgysfTfTuzTC4LNQ","crv":"P-256","d":"QS-q9RzH1u2Oj8gDiUzLk1qpGxZjKSf-3Z1oKCRL_jQ"}'),
-
-  SESSION_KEY: z.string().default('bJVS+Dx03A3QWWfW3A5Om5DGx1GKptx+1IGAXzOTpw8='),
-  SESSION_TTL: z.number().default(((60 * 60) * 24) * 5), // expire in 5 days
 
   SENTRY_DSN: z.string().or(z.undefined()),
 
